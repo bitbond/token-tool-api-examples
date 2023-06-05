0b8414626fdaeba237e683ff3c084a3d7e53cdfb
Bartek Rogalski <bartek.rogalski@gmail.com>
2023-06-05 17:37:49 +0200

    Fireblocks token creation

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# token-tool-api-example
-Collection of scripts illustrating API interaction with Token Tool contracts
+# Bitbond Token Tool API examples
+A collection of script samples illustrating interacting with Bitbond Token Tool
+contracts.
+
+## Requirements
+- Node 18.16.0
+- yarn v1
+
+## Setup
+
+Copy API user key and secret created with your custody provider (Fireblocks) into this directory.
+Refer to `fireblocks_key.example` and `fireblocks_secret.example` to verify that
+the format of the key matches values expected by the script.
+
+Install node packages:
+
+```
+yarn
+```
+
+Configure token which is going to be deployed by editing `config.ts` file.
+
+## Fireblocks
+
+1. Review token configuration in `config.ts`.
+1. Review Fireblocks configuration in `config.ts` to select vault and chain.
+1. To deploy the token run:
+```
+npx ts-node ./fireblocks/deployToken.ts <fireblocks_key_file> <fireblocks_secret_file>
+```
---
+++ config.ts
@@ -0,0 +1,57 @@
+// New token configuration
+export const token = {
+  name: "ABC-2 Token",
+  symbol: "ABC-2",
+  // Initial supply of tokens to be minted with token creation
+  initialSupply: "100",
+  decimals: "18",
+  // Owner address of deployed token contract.
+  // It can be the address of vault account signing the transaction,
+  // but it can also be any other address that will own the contract after creation.
+  issuerAddress: "0x20F0022FBE21a1CC1304C25Da20Cd8DF66289da4",
+  flags: {
+    // Additional tokens can be minted after contract creation to increase supply
+    _isMintable: false,
+    // Token and all associated operations can be halted
+    _isPausable: false,
+    // Tokens can be burned after contract creation to decrease supply
+    _isBurnable: false,
+    // Addresses can be blacklisted by the owner,
+    // preventing them from direct interaction with tokens
+    _isBlacklistEnabled: false,
+    // A hash or URI can be used to reference documentation of the asset
+    // It can be updated after creation
+    _isDocumentAllowed: false,
+    // Token transfers are only possible to whitelisted addresses
+    // or if the token is freely transferable.
+    _isWhitelistEnabled: false,
+    // If  tokens that an address can hold
+    _isMaxAmountOfTokensSet: false,
+    // Tokens can be force-transferred by the owner,
+    // cannot be deactivated after creation
+    _isForceTransferAllowed: false,
+  },
+  // If _isMaxAmountOfTokensSet is true,
+  // this specifies the maximum number of tokens that an address can hold
+  balanceLimit: 0,
+  // If _isDocumentAllowed is true,
+  // this specifies the document URI that can be updated by the owner
+  documentUri: "",
+}
+
+export const fireblocksParams = {
+  // Vault used to sign the transaction,
+  // Could be any vault with enough balance to cover the transaction fee
+  // To set contract owner, use token.issuerAddress parameter.
+  vaultId: "0",
+  // Determines the network where transaction is executed,
+  // refer to Fireblocks documentation for other native asset codes
+  assetId: "MATIC_POLYGON_MUMBAI",
+  // Any string
+  note: "Deploying FullFeatureToken",
+  // Should always be 0, amount of native currency to send with the call
+  amount: "0"
+}
+
+// Provided by Bitbond
+export const factoryAddress = "0x88777bcCb752B20245400049021CB47b8fbCf640";
