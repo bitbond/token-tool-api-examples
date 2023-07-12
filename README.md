75e008d78477fbcc49cacc22827d83c9d7ac4709
Bartek Rogalski <bartek.rogalski@gmail.com>
2023-07-12 15:55:54 +0200

    Add token lifecycle events for Fireblocks

--- README.md
+++ README.md
@@ -6,28 +6,52 @@ JavaScript by removing type definitions.
 Example token deployment: [Block explorer](https://mumbai.polygonscan.com/tx/0xd366367005e841cc97e0ffd02002114dfae41222559116b54f56298d45bf057d)
 
 ## Requirements
-- Node 18.16.0
-- yarn v1
+Recommended:
+- Node.js 18.16.0 or higher
+- npm 9.5.1 or higher
 
 ## Setup
-
-Copy API user key and secret created with your custody provider (Fireblocks) into this directory.
-Refer to `fireblocks_key.example` and `fireblocks_secret.example` to verify that
-the format of the key matches values expected by the script.
-
-Install node packages:
+1. Install [Node.js](https://nodejs.org/en) and [npm](https://npmjs.com).
+Exact installation steps depend on the platfrom, please refer to the documentation for instructions.
+1. Copy API key and private key created with your custody provider (Fireblocks) into this directory.
+Refer to `fireblocks_key.example` and `fireblocks_secret.example` files to verify that
+the format of the key matches values expected by the scripts.
+1. Install node packages:
 
 ```
-yarn
+npm install
 ```
-
-Configure token which is going to be deployed by editing `config.ts` file.
 
 ## Fireblocks
+### Deploying new token
 
-1. Review token configuration in `config.ts`.
-1. Review Fireblocks configuration in `config.ts` to select Fireblocks vault and chain.
+1. Customize token configuration in `fireblocks/deployToken.ts`.
+1. Customize Fireblocks configuration in `fireblocks/deployToken.ts` to select
+Fireblocks vault and chain that will used for the deployment.
 1. To deploy the token run:
 ```
-npx ts-node ./fireblocks/deployToken.ts <fireblocks_key_file> <fireblocks_secret_file>
+npx ts-node ./fireblocks/deployToken.ts
+```
+### Transferring tokens
+
+1. Customize transfer parameters in `fireblocks/transferTokens.ts`.
+1. To transfer the tokens run:
+```
+npx ts-node ./fireblocks/transferTokens.ts
+```
+
+### Minting tokens
+
+1. Customize minting parameters in `fireblocks/mintTokens.ts`.
+1. To mint the tokens run:
+```
+npx ts-node ./fireblocks/mintTokens.ts
+```
+
+### Buring tokens
+
+1. Customize burning parameters in `fireblocks/burnTokens.ts`.
+1. To burn the tokens run:
+```
+npx ts-node ./fireblocks/burnTokens.ts
 ```
--- fireblocks/deployToken.ts
+++ fireblocks/deployToken.ts
@@ -5,7 +5,67 @@ import { Deferrable } from "@ethersproject/properties";
 import type { ContractInterface, BytesLike, ContractFactory } from "ethers";
 import tokenArtifact from "../assets/FullFeatureToken.json";
 import fs from "fs";
-import { token, fireblocksParams, factoryAddress } from "../config";
+
+// Edit the values below according to your needs
+// New token configuration
+export const token = {
+  name: "ABC-0 Token",
+  symbol: "ABC-0",
+  // Initial supply of tokens to be minted with token creation
+  initialSupply: "100",
+  // Number of decimals to be used by the token
+  decimals: "18",
+  // Owner address of deployed token contract.
+  // It can be the address of vault account signing the transaction,
+  // but it can also be any other address that will own the contract after creation.
+  issuerAddress: "0x...",
+  flags: {
+    // Additional tokens can be minted after contract creation to increase supply
+    _isMintable: true,
+    // Token and all associated operations can be halted
+    _isPausable: true,
+    // Tokens can be burned after contract creation to decrease supply
+    _isBurnable: true,
+    // Addresses can be blacklisted by the owner,
+    // preventing them from direct interaction with tokens
+    _isBlacklistEnabled: false,
+    // A hash or URI can be used to reference documentation of the asset
+    // It can be updated after creation
+    _isDocumentAllowed: false,
+    // Token transfers are only possible to whitelisted addresses
+    // or if the token is freely transferable.
+    _isWhitelistEnabled: false,
+    // Limit the number of tokens that an address can hold
+    _isMaxAmountOfTokensSet: false,
+    // Tokens can be force-transferred by the owner,
+    // cannot be deactivated after creation
+    _isForceTransferAllowed: false,
+  },
+  // If _isMaxAmountOfTokensSet is true,
+  // this specifies the maximum number of tokens that an address can hold
+  balanceLimit: 0,
+  // If _isDocumentAllowed is true,
+  // this specifies the document URI that can be updated by the owner
+  documentUri: "",
+};
+
+export const fireblocksParams = {
+  // Vault ID that is used to sign the transaction,
+  // Could be any vault with enough balance to cover the transaction fee
+  // To transfer ownership of contract on its creation use token.issuerAddress
+  // parameter.
+  vaultId: "0",
+  // Determines the network where transaction is executed,
+  // refer to Fireblocks documentation for other native asset codes
+  assetId: "MATIC_POLYGON_MUMBAI",
+  // Any string
+  note: "Deploying token",
+  // Should always be 0, amount of native currency to send with the call
+  amount: "0"
+};
+
+// Provided by Bitbond
+export const factoryAddress = "0x88777bcCb752B20245400049021CB47b8fbCf640";
 
 type ContractArtifact = {
   abi: ContractInterface;
@@ -13,13 +73,13 @@ type ContractArtifact = {
 };
 
 const fireblocks = () => {
-  const fireblocksApiKey = fs.readFileSync(process.argv[2], "utf-8").trim();
-  const fireblocksApiSecret = fs.readFileSync(process.argv[3], "utf-8").trim();
+  const fireblocksApiKey = fs.readFileSync("./fireblocks_api_key", "utf-8").trim();
+  const fireblocksPrivateKey = fs.readFileSync("./fireblocks_private_key", "utf-8").trim();
 
-  if (!fireblocksApiKey || !fireblocksApiSecret) {
+  if (!fireblocksApiKey || !fireblocksPrivateKey) {
     throw new Error("Missing Fireblocks API key or secret");
   }
-  return new FireblocksSDK(fireblocksApiSecret, fireblocksApiKey);
+  return new FireblocksSDK(fireblocksPrivateKey, fireblocksApiKey);
 };
 
 const getBytecode =
